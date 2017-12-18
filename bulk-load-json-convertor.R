85dac3ddfacb6689e19343242a5761c09f1dfdc8
Larry-Hignight <larry.hignight@gmail.com>
2017-12-17 16:15:34 -0800

    Changed the bulk load script to always use 'doc' as the _type value.

--- bulk-load-json-convertor.R
+++ bulk-load-json-convertor.R
@@ -8,12 +8,12 @@ library(jsonlite)
 # {"index":{}}
 # {"is_inactive": "0", "created": "2017-01-01 12:30:00", "tenant_id": "1", "book_key": "970851715", "created_by_user": "449", "published_book_build_id": "NULL", "published": "2017-01-01 12:30:00", "publisher_id": "1", "id": "383"}
 # 
-# Note - This script uses the index value for 'mapping type'
+# Note - This script always sets the 'mapping type' to "doc"
 # See https://www.elastic.co/guide/en/elasticsearch/reference/6.x/removal-of-types.html
 #
 bulk_load_format <- function(x, index = NA) {
   metadata <- str_c('{"index":{', 
-                    ifelse(is.na(index), "", metadata <- str_c('"_index": "', index, '", "_type": "', index, '"')),
+                    ifelse(is.na(index), "", metadata <- str_c('"_index": "', index, '", "_type": "doc"')),
                     '}}')
   rows <- sapply(1:nrow(x), function(n) toJSON(x[n, ]))
   rows <- str_sub(rows, start = 2, end = str_length(rows) - 1)
